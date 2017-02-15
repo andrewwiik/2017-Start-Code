c935bb7ae3f5edef1807e4d29cb62c3094e224d7
Aaron Kiedes <akiedes@vtsd.com>
2017-02-15 13:20:14 -0500

    Added "Components.java" in order to move component object definitions to a separate file and clean up Robot.java a bit.  Began working on ClimberCMD.java.  Made a few of the names a bit simpler, began reworking the algorithm (Still in progress.

---
+++ 2017-Start-Code/src/org/usfirst/frc/team1989/robot/Components.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team1989.robot;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
+public final class Components {
+	
+	static CANTalon1989 driveFrontLeft = new CANTalon1989(3);
+	static CANTalon1989 driveFrontRight = new CANTalon1989(9);
+	static CANTalon1989 driveBackLeft = new CANTalon1989(7);
+	static CANTalon1989 driveBackRight = new CANTalon1989(25);
+	static CANTalon1989 climberLeft = new CANTalon1989(4);
+	static CANTalon1989 climberRight = new CANTalon1989(2);
+	static CANTalon1989 gearMotor = new CANTalon1989(6);
+	
+	// Instantiating Servo
+	static Servo servoX = new Servo(0);
+	static Servo servoY = new Servo(1);
+
+	// Instantiating Joysticks
+	static JsScaled driveStick = new JsScaled(0);
+	static JsScaled uStick = new JsScaled(1);//The uStick will stand for the utility joystick responsible for shooting and arm movement
+	
+	// Instantiate Gyro (Doesn't work)
+	static Gyro gyro;
+
+}
--- 2017-Start-Code/src/org/usfirst/frc/team1989/robot/Robot.java
+++ 2017-Start-Code/src/org/usfirst/frc/team1989/robot/Robot.java
@@ -23,19 +23,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot implements cmd{
 	
-	
-	CANTalon1989 driveFrontLeft = new CANTalon1989(3);
-	CANTalon1989 driveFrontRight = new CANTalon1989(9);
-	CANTalon1989 driveBackLeft = new CANTalon1989(7);
-	CANTalon1989 driveBackRight = new CANTalon1989(25);
-	CANTalon1989 climberLeft = new CANTalon1989(4);
-	CANTalon1989 climberRight = new CANTalon1989(2);
-	CANTalon1989 gearMotor = new CANTalon1989(6);
-	
 	Integer tmpint = new Integer(0);
 
-
-	
 	double driveramp = 6.0;
 	public String type = ""; // holds class type
 	public static double Kp = 0.03; // const for multiplying gyro angle
@@ -47,22 +36,10 @@ public class Robot extends IterativeRobot implements cmd{
 	// Instantiating Timer
 	Timer t1 = new Timer();
 	
-
-	// Instantiating Servo
-	Servo servoX = new Servo(0);
-	Servo servoY = new Servo(1);
-
-	// Instantiating Joysticks
-	JsScaled driveStick = new JsScaled(0);
-	JsScaled uStick = new JsScaled(1);//The uStick will stand for the utility joystick responsible for shooting and arm movement
-	
-	Gyro gyro;
-
-
-	MecDriveCmd mDrive = new MecDriveCmd(driveFrontLeft, driveBackLeft, driveFrontRight, driveBackRight, driveStick);
-	CameraControl camControl = new CameraControl(servoX, servoY, driveStick);
-	GearPushCmd gearPusher = new GearPushCmd(gearMotor, driveStick);
-	ClimberCmd climber = new ClimberCmd(climberLeft, climberRight, driveStick);
+	MecDriveCmd mDrive = new MecDriveCmd(Components.driveFrontLeft, Components.driveBackLeft, Components.driveFrontRight, Components.driveBackRight, Components.driveStick);
+	CameraControl camControl = new CameraControl(Components.servoX, Components.servoY, Components.driveStick);
+	GearPushCmd gearPusher = new GearPushCmd(Components.gearMotor, Components.driveStick);
+	ClimberCmd climber = new ClimberCmd(Components.climberLeft, Components.climberRight, Components.driveStick);
 	@Override
 	public void robotInit() {
 		SharedStuff.cmdlist.add(mDrive);
@@ -116,27 +93,27 @@ public class Robot extends IterativeRobot implements cmd{
 		}
 		
 	
-	// From Mr. P - Count the number of times we run "periodically" during a second.
-	/*if(t1.get() >= 1){
-		//System.out.println(mDrive.driveBackLeft.getEncPosition());
-		//System.out.println(mDrive.driveBackRight.getEncPosition());
-		//System.out.println("count " + tmpint);
-		tmpint = 0;
-		t1.stop();
-		t1.reset();
-		t1.start();
-	}*/
-
-	// Drive Code Test
-	/*if(driveStick.getRawButton(6) == true){	
-
-
-		climberLeft.set(-0.75);
-		climberRight.set(-0.75);
-	} else{
-		climberLeft.set(0);
-		climberRight.set(0);
-	}*/
+		// From Mr. P - Count the number of times we run "periodically" during a second.
+		/*if(t1.get() >= 1){
+			//System.out.println(mDrive.driveBackLeft.getEncPosition());
+			//System.out.println(mDrive.driveBackRight.getEncPosition());
+			//System.out.println("count " + tmpint);
+			tmpint = 0;
+			t1.stop();
+			t1.reset();
+			t1.start();
+		}*/
+	
+		// Drive Code Test
+		/*if(driveStick.getRawButton(6) == true){	
+	
+	
+			climberLeft.set(-0.75);
+			climberRight.set(-0.75);
+		} else{
+			climberLeft.set(0);
+			climberRight.set(0);
+		}*/
 	
 		
 	
