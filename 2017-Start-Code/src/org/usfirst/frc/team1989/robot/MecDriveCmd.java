5d35c454258e97561227883a2435ee87afc1795c
mrkiedes <akiedes@vtsd.com>
2017-02-17 16:23:45 -0500

    Updates to classes and logic.  Worked on with programming team.

--- 2017-Start-Code/src/org/usfirst/frc/team1989/robot/MecDriveCmd.java
+++ 2017-Start-Code/src/org/usfirst/frc/team1989/robot/MecDriveCmd.java
@@ -5,18 +5,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.ctre.CANTalon;
 public class MecDriveCmd implements cmd {
-	 
 	
-	
+	// Instantiate Basic Components
 	CANTalon1989 driveFrontLeft;
 	CANTalon1989 driveFrontRight;
 	CANTalon1989 driveBackLeft;//encoder motor
 	CANTalon1989 driveBackRight;// encoder motor
-	
 	RobotDrive driveTrain;
 	JsScaled driveStick;
 	
-;
+	// Par of the Encoder Test (needs class scope, or it would go in test init)
 	Integer encoderLeftCount;
 	Integer encoderRightCount;
 	
@@ -33,65 +31,69 @@ public class MecDriveCmd implements cmd {
 		this.driveBackRight.setInverted(true);
 	}
 	
-	
+	// Check the values of each encorder and display it to the smart dashboard
+	// This is part of the test code for the encoder based drive
 	public void encoderCheck(){
+		
+		// If going straight get encoder values.  Otherwise set them to 0.
 		if(Math.abs(driveStick.sgetTwist()) < 0.1 && Math.abs(driveStick.sgetX()) < 0.1 && Math.abs(driveStick.sgetY()) > 0.1){
 			encoderLeftCount = driveBackLeft.getEncPosition();
 			encoderRightCount = driveBackRight.getEncPosition();
-			Components.writemessage.setmessage(0, encoderLeftCount.toString());
-			Components.writemessage.setmessage(1, encoderRightCount.toString());
-			
-		} else if(Math.abs(driveStick.sgetTwist()) > 0.1 || Math.abs(driveStick.sgetX()) > 0.1){
+		} 
+		if(Math.abs(driveStick.sgetTwist()) > 0.1 || Math.abs(driveStick.sgetX()) > 0.1){
 			encoderLeftCount = 0;
 			encoderRightCount = 0;
-			Components.writemessage.setmessage(0, encoderLeftCount.toString());
-			Components.writemessage.setmessage(0, encoderRightCount.toString());
+			
 		}
+		// Output data to the dash
+		Components.writemessage.setmessage(0, encoderLeftCount.toString());
+		Components.writemessage.setmessage(0, encoderRightCount.toString());
 			
 	}
 	
+	public void encoderDrive(){
+		// When going straight, attempt to make any necessary corrections
+		if(driveStick.sgetTwist() < 0.1 && driveStick.sgetX() < 0.1 && driveStick.sgetY() > 0.1){
+			
+			
+			if(encoderLeftCount < encoderRightCount){
+				driveTrain.mecanumDrive_Cartesian(driveStick.sgetX()/2, driveStick.sgetY()/2, -0.05, 0);
+			}
+			if(encoderLeftCount > encoderRightCount){
+				driveTrain.mecanumDrive_Cartesian(driveStick.sgetX()/2, driveStick.sgetY()/2, 0.05, 0);
+			}
+			// Why is this here? I understand the equals condition, but inside you are checking the exact condition above that would be tripped.  
+			/*else{
+				if(encoderLeftCount < encoderRightCount){
+					driveTrain.mecanumDrive_Cartesian(driveStick.sgetX()/2, driveStick.sgetY()/2, driveStick.sgetTwist(), 0);
+				}
+			}*/
+		} /*else{
+			driveTrain.mecanumDrive_Cartesian(driveStick.sgetX()/2, driveStick.sgetY()/2, driveStick.sgetTwist(), 0);
+		}*/
+			
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
 	public void autonomousInit(){}
 	public void autonomousPeriodic() {
 		driveTrain.mecanumDrive_Cartesian(driveStick.pX, driveStick.pY, driveStick.pTwist,0);//Last 0 is gyro angle needs to be checked if we get one
 	}
 	public void teleopInit(){}
 	public void teleopPeriodic(){
-			encoderCheck();
-			
-			
-			driveTrain.mecanumDrive_Cartesian(driveStick.sgetX(), driveStick.sgetY(), driveStick.sgetTwist(), 0);
-			Components.writemessage.updatedash();
+		
+		driveTrain.mecanumDrive_Cartesian(driveStick.sgetX(), driveStick.sgetY(), driveStick.sgetTwist(), 0);
+		Components.writemessage.updatedash();
 	}
 	public void testInit(){}
+	
+	
+	
 	public void testPeriodic(){
-		
-		if(driveStick.sgetTwist() < 0.1 && driveStick.sgetX() < 0.1 && driveStick.sgetY() > 0.1){
-			if(encoderLeftCount < encoderRightCount){
-				driveTrain.mecanumDrive_Cartesian(driveStick.sgetX()/2, driveStick.sgetY()/2, -0.05, 0);
-			}
-			else if(encoderLeftCount > encoderRightCount){
-				driveTrain.mecanumDrive_Cartesian(driveStick.sgetX()/2, driveStick.sgetY()/2, 0.05, 0);
-			}
-			else{
-				if(encoderLeftCount < encoderRightCount){
-					driveTrain.mecanumDrive_Cartesian(driveStick.sgetX()/2, driveStick.sgetY()/2, driveStick.sgetTwist(), 0);
-				}
-			}
-		} else{
-			driveTrain.mecanumDrive_Cartesian(driveStick.sgetX()/2, driveStick.sgetY()/2, driveStick.sgetTwist(), 0);
-		}
+		// Display encoder values
+		encoderCheck();
+		encoderDrive();
+		driveTrain.mecanumDrive_Cartesian(driveStick.sgetX(), driveStick.sgetY(), driveStick.sgetTwist(), 0);
+		Components.writemessage.updatedash();
 	}
 	
 
